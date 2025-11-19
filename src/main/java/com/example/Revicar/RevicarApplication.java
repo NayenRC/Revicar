575cd8cdaab162728b0e20c7d09a88e4352e1999
Nayen Roman <nay.roman@duocuc.cl>
2025-11-19 00:32:10 -0300

    primer commit

---
+++ src/main/java/com/example/Revicar/RevicarApplication.java
@@ -0,0 +1,13 @@
+package com.example.Revicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevicarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevicarApplication.class, args);
+	}
+
+}
